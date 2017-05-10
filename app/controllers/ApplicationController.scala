4c06ef3e61306f98e92c00edda1dbea55660d650
Emilio Coppola <coppolaemilio@gmail.com>
2017-05-10 22:52:10 +0200

    loading static files

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -1,8 +1,11 @@
 package controllers
 
 import play.api.mvc.{Action, Controller}
+import play.api.mvc.Results._
 
 class ApplicationController extends Controller {
+  val pageNotFound = NotFound(<h1>Page not found</h1>)
+
   def index = Action {
     Ok(views.html.page.index())
   }
